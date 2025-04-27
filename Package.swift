f63c5da94dfebc9762e2c889f1a8481dc8040d68
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2025-04-27 15:04:10 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,17 +2,17 @@
 import PackageDescription
 
 let package = Package(
-  name: "IzziAuth",
+  name: "IzziSession",
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
-      name: "IzziAuth",
-      targets: ["IzziAuth"]),
+      name: "IzziSession",
+      targets: ["IzziSession"]),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "IzziAuth")
+      name: "IzziSession")
   ]
 )
